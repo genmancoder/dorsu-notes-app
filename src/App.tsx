f8b82a459cf21dbdc9b4a41fa3f3bb5ee33f374d
genmancoder <genmansystems@gmail.com>
2022-08-21 09:12:04 +0800

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+    Simple React Typescript Tailwind Sample
+  </h1>
+  );
+}
+
+export default App;
