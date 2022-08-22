e0379ec80c412c8c2ecdd360ba438db69a2eac1e
genmancoder <genmansystems@gmail.com>
2022-08-23 05:18:18 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <header>
       <nav className="flex flex-col md:flex-row justify-between items-center bg-blue-400 text-white px-20 md:h-12">
-          <h1>DORSU Notes</h1>
+          <h1>DORSU Notes - Dev Version.</h1>
           
           <ul id="mobile-menu" className="flex flex-col md:flex-row md:flex justify-between items-center w-56 sm:w-auto">
             <a href="#">
