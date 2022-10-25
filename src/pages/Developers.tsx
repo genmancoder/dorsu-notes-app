5824136d994f888f15b730cb374999076b678c5f
Jonathan Jules L. Serato <112166112+jyzkng@users.noreply.github.com>
2022-10-25 23:38:27 +0800

    Add Profile

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -5,20 +5,22 @@ const Developers = () => {
     <div className='container mx-auto p-5'>
         <h1>Developers</h1>
         <div className='p-5 m-5 grid grid-cols-6'>            
-                {/* Add your profile here. You can upload your image to the repo under "images"  directory
-                    and link it here.                
-                */} 
-          
-               <img src="images\jules.jpg" width="100" height="100">
-
+                {/* grid item */}
+                <div className="flex flex-wrap justify-center">
+                    <div className="w-64 sm:w-64 px-4">
+                    <img src="images\jules.jpg" width="90" height="90">
+                    <h3 className='font-bold mr-1'>Jonathan Jules L. Serato </h3>
+                    <p className='font-thin italic'> Developer</p>
+                </div>           
+                {/* grid item */}               
 
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
                         <img src="https://avatars.githubusercontent.com/u/74034797?s=400&u=8d250db9bceb4f7415526ea5b276c203f10e3fa9&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
                     </div>
-                    <h3 className='font-bold mr-1'>Jonathan Jules L. Serato </h3>
-                    <p className='font-thin italic'> Developer</p>
+                    <h3 className='font-bold mr-1'>John Paulin, </h3>
+                    <p className='font-thin italic'> Lead Developer</p>
                 </div>           
                 {/* grid item */}               
                
