5fdb1c78bd98acf004da76bf3e3e4464495637a0
lvlytorno <111828209+lvlytorno@users.noreply.github.com>
2022-10-25 21:57:45 +0800

    add developers profile

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -3,25 +3,32 @@ import React from 'react'
 const Developers = () => {
   return (
     <div className='container mx-auto p-5'>
-        <h1>Developers</h1>
-        <div className='p-5 m-5 grid grid-cols-6'>            
-                {/* Add your profile here. You can upload your image to the repo under "images"  directory
+      <h1>Developers</h1>
+      <div className='p-5 m-5 grid grid-cols-6'>
+        {/* Add your profile here. You can upload your image to the repo under "images"  directory
                     and link it here.                
                 */}
-
-
-                {/* grid item */}
-                <div className="flex flex-wrap justify-center">
-                    <div className="w-64 sm:w-64 px-4">
-                        <img src="https://avatars.githubusercontent.com/u/74034797?s=400&u=8d250db9bceb4f7415526ea5b276c203f10e3fa9&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
-                    </div>
-                    <h3 className='font-bold mr-1'>John Paulin, </h3>
-                    <p className='font-thin italic'>Lead Developer</p>
-                </div>           
-                {/* grid item */}               
-               
-
+        <div className="flex flex-wrap justify-center">
+          <div className="w-64 sm:w-64 px-4">
+            <img src="src/images/Lovely_Torno.jpg" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+          </div>
+          <h3 className='font-bold mr-1'>Lovely Torno, </h3>
+          <p className='font-thin italic'>Developer</p>
         </div>
+
+
+        {/* grid item */}
+        <div className="flex flex-wrap justify-center">
+          <div className="w-64 sm:w-64 px-4">
+            <img src="https://avatars.githubusercontent.com/u/74034797?s=400&u=8d250db9bceb4f7415526ea5b276c203f10e3fa9&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+          </div>
+          <h3 className='font-bold mr-1'>John Paulin, </h3>
+          <p className='font-thin italic'>Lead Developer</p>
+        </div>
+        {/* grid item */}
+
+
+      </div>
     </div>
   )
 }
