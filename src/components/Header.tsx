073d72197b6e11a159e34ec2bda7bc4dca6bff40
genmancoder <genmansystems@gmail.com>
2022-10-25 13:54:49 +0800

    Dev section

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Outlet, Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <header>
+        <nav className="flex flex-col md:flex-row justify-between items-center bg-blue-400 text-white px-20 md:h-12">
+            <h1>DORSU Notes - Dev Version.</h1>
+            
+            <ul id="mobile-menu" className="flex flex-col md:flex-row md:flex justify-between items-center w-56 sm:w-auto">
+              <li className='mr-4'>
+                <Link to="/">Home</Link>
+              </li>
+              <li className='mr-4'>
+                <Link to="/developers">Developers</Link>
+              </li>
+              <li className='mr-4'>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+        </nav>
+
+    </header>
+  )
+}
+
+export default Header
---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='container mx-auto p-5'>
+        About Section
+    </div>
+  )
+}
+
+export default About
