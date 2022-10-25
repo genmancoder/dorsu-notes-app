00ed3b9c2ca88b4191365f076ebf5219685ccd69
Jonathan Jules L. Serato <111748048+patrickheyres@users.noreply.github.com>
2022-10-26 00:55:47 +0800

    Add Profile

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -4,10 +4,16 @@ const Developers = () => {
   return (
     <div className='container mx-auto p-5'>
         <h1>Developers</h1>
-        <div className='p-5 m-5 grid grid-cols-6'>            
-                {/* Add your profile here. You can upload your image to the repo under "images"  directory
-                    and link it here.                
-                */}
+        <div className='p-5 m-5 grid grid-cols-6'>
+
+        <div className="flex flex-wrap justify-center">
+                    <div className="w-64 sm:w-64 px-4">
+
+                    <img src="images\patrick.jpg" width="100" height="100">
+                    </div>
+                    <h3 className='font-bold mr-1'>Patrick Gerald W. Heyres </h3>
+                    <p className='font-thin italic'>Developer</p>
+                </div>     
 
 
                 {/* grid item */}
