866425cd39467c7bff2c58239c55fc899f387ac5
Edster29 <69906506+Edster29@users.noreply.github.com>
2022-10-26 07:18:21 -0700

    Update Developers.tsx

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -13,7 +13,7 @@ const Developers = () => {
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
-                        <img src="images/Edster.jpg" alt="Picture of Edster" classNamee="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                        <img src="images/Edster.jpg" alt="Picture of Edster" className="shadow rounded-full max-w-full h-auto align-middle border-none" />
                     </div>
                     <h3 className='font-bold mr-1'>Edster Manuel M. Mose, </h3>
                     <p className='font-thin italic'>Student Developer</p>
