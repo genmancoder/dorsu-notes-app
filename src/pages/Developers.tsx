c4b21e1755bd77819d0c471f869bdfd17b36dc7d
Rassey04 <111989159+Rassey04@users.noreply.github.com>
2022-10-26 22:41:31 +0800

    Update Developers.tsx

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -15,8 +15,8 @@ const Developers = () => {
                     <div className="w-64 sm:w-64 px-4">
                         <img src="https://avatars.githubusercontent.com/u/74034797?s=400&u=8d250db9bceb4f7415526ea5b276c203f10e3fa9&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
                     </div>
-                    <h3 className='font-bold mr-1'>Rassey L. Perez, </h3>
-                    <p className='font-thin italic'>Php Developer</p>
+                    <h3 className='font-bold mr-1'>ohn Paulin, </h3>
+                    <p className='font-thin italic'>Lead Developerr</p>
                 </div>           
                 {/* grid item */}               
                
