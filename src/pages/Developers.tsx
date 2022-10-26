8d1ebe8be6a68a977fc83a8537899af00d10f506
Aran Glore C. Peliña <pelina.courage@gmail.com>
2022-10-26 20:39:33 +0800

    Update developers.tsx

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -13,9 +13,9 @@ const Developers = () => {
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
-                        <img src="https://avatars.githubusercontent.com/u/74034797?s=400&u=8d250db9bceb4f7415526ea5b276c203f10e3fa9&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                        <img src="https://avatars.githubusercontent.com/u/69896582?s=400&u=9e9492c195c37e3cb3bca6d08da58ed75760c18d&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
                     </div>
-                    <h3 className='font-bold mr-1'>Rassey L. Perez, </h3>
+                    <h3 className='font-bold mr-1'>Aran Glore C. Peliña </h3>
                     <p className='font-thin italic'>Php Developer</p>
                 </div>           
                 {/* grid item */}               
